7ab233aca97dcdefa8dec920fb42c1e1989d37ab
HDCODE <119462812+hdcodetj@users.noreply.github.com>
2024-01-25 19:53:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector('.texto__paragrafo')
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
 let numeroSecreto = gerarNumero()
-
+let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -13,11 +13,27 @@ exibirTextoNaTela('h1', 'Jogo Secreto');
 exibirTextoNaTela('.texto__paragrafo', 'Escolha um texto de 1 a 100');
 
 function verificarChute() {
-    let chute = document.querySelector('.container__input').value
-    console.log(numeroSecreto)
-    console.log(chute == numeroSecreto);
+    let chute = document.querySelector('.container__input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', '🥳 Parabéns!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto, que é ${numeroSecreto}. Com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    } else if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', `O número secreto é menor`);
+    } else {
+        exibirTextoNaTela('p', `O número secreto é maior`);
+    tentativas++
+    }
+    limparChute()
 }
 
 function gerarNumero() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparChute() {
+    chute = document.querySelector('.container__input');
+    chute.value = "";
 }
